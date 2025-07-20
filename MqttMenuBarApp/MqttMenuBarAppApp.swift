875b6d445a4cc8f0ae05b7744cd558465651bb10
saihgupr <121105543+saihgupr@users.noreply.github.com>
2025-07-20 10:57:26 -0400

    Initial Commit

---
+++ MqttMenuBarApp/MqttMenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MqttMenuBarAppApp.swift
+//  MqttMenuBarApp
+//
+//  Created by Chris LaPointe on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MqttMenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
